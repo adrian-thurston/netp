76cb9fb3c8662f50978bbe8346065de5a2543dea
Adrian Thurston <thurston@complang.org>
2014-11-12 11:56:04 -0600

    work on object/attribute declaration
    * Respect store/show keywords in attributes (only generate what is asked for).
    * Added create_[object] function.
    * Added grammar defs for attribute arguments.

--- test/kern1/attribute.c
+++ test/kern1/attribute.c
@@ -5,11 +5,6 @@ struct ring
 	struct kobject kobj;
 };
 
-struct device
-{
-	struct kobject kobj;
-};
-
 static int n;
 
 static ssize_t ring_foo_show( struct ring *obj, char *buf )
@@ -30,18 +25,6 @@ static ssize_t ring_bar_show( struct ring *obj, char *buf )
 }
 
 static ssize_t ring_bar_store( struct ring *obj,
-		const char *buf, size_t count )
-{
-	sscanf( buf, "%du", &n );
-	return count;
-}
-
-static ssize_t device_tracks_show( struct device *obj, char *buf )
-{
-	return sprintf( buf, "%d\n", n );
-}
-
-static ssize_t device_tracks_store( struct device *obj,
 		const char *buf, size_t count )
 {
 	sscanf( buf, "%du", &n );
--- test/kern1/kern1.gf
+++ test/kern1/kern1.gf
@@ -1,9 +1,4 @@
 module ring;
 
-kobj device
-{
-	attribute show store tracks;
-};
-
 attribute show store foo;
 attribute show store bar;
