6b1659498053815a2f038de94a2dbf34961dcc17
Adrian Thurston <thurston@complang.org>
2014-11-14 13:59:04 -0600

    shuttling packets back and forth
    works as long as interfaces are manually put into promiscuous mode

--- src/attribute.c
+++ src/attribute.c
@@ -1,5 +1,7 @@
-#include <linux/netdevice.h>
 #include "attribute.h"
+
+#include <linux/netdevice.h>
+#include <linux/rtnetlink.h>
 
 struct filter
 {
@@ -9,9 +11,31 @@ struct filter
 struct link
 {
 	struct kobject kobj;
+	struct net_device *inside, *outside;
 };
 
 struct link *lo = 0;
+
+rx_handler_result_t filter_handle_frame( struct sk_buff **pskb )
+{
+	struct sk_buff *skb = *pskb;
+
+	if ( skb->dev == lo->inside ) {
+		skb->dev = lo->outside;
+		skb_push(skb, ETH_HLEN);
+		dev_queue_xmit( skb );
+	}
+	else if ( skb->dev == lo->outside ) {
+		skb->dev = lo->inside;
+		skb_push(skb, ETH_HLEN);
+		dev_queue_xmit( skb );
+	}
+	else {
+		kfree_skb( skb );
+	}
+
+	return RX_HANDLER_CONSUMED;
+}
 
 static ssize_t filter_add_store( struct filter *obj,
 		const char *buf, size_t count )
@@ -21,19 +45,29 @@ static ssize_t filter_add_store( struct filter *obj,
 	struct net_device *outside_dev;
 
 	sscanf( buf, "%s %s %s", link, inside, outside );
+
+
 	create_link( &lo, &root_obj->kobj );
 
 	inside_dev = dev_get_by_name( &init_net, inside );
 	if ( inside_dev ) {
 		printk( "found inside\n" );
-		dev_put( inside_dev );
 	}
 
 	outside_dev = dev_get_by_name( &init_net, outside );
 	if ( outside_dev ) {
 		printk( "found outside\n" );
-		dev_put( outside_dev );
 	}
+
+	lo->inside = inside_dev;
+	lo->outside = outside_dev;
+
+	rtnl_lock();
+
+	netdev_rx_handler_register( inside_dev, filter_handle_frame, 0 );
+	netdev_rx_handler_register( outside_dev, filter_handle_frame, 0 );
+
+	rtnl_unlock();
 
 	return count;
 }
@@ -44,8 +78,11 @@ static ssize_t filter_del_store( struct filter *obj,
 	char link[32];
 	sscanf( buf, "%s", link );
 
-	if ( lo ) 
+	if ( lo ) {
+		dev_put( lo->inside );
+		dev_put( lo->outside );
 		kobject_put( &lo->kobj );
+	}
 	return count;
 }
 
