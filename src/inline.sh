020beae204730669197d44e2b48a452ea619dbe5
Adrian Thurston <thurston@colm.net>
2016-12-25 11:36:50 -0500

    some base code for TPROXY with inline module
    not made to work yet

---
+++ src/inline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+set -x
+
+for fn in /proc/sys/net/ipv4/conf/*/rp_filter; do echo 0 > $fn; done
+
+iptables -t mangle -N DIVERT
+
+iptables -t mangle -A PREROUTING -p tcp -m socket -j DIVERT
+iptables -t mangle -A DIVERT -j MARK --set-mark 101
+iptables -t mangle -A DIVERT -j ACCEPT
+
+ip rule add fwmark 101 lookup 101
+ip route add local default dev eth1 table 101
+
+iptables -t mangle -A PREROUTING -p tcp -m tcp --dport 443 -j TPROXY  --on-port 4430 --tproxy-mark 101/101
