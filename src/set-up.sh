7c57de3dd26c10c7101076f58904eac09e5fbb9f
Adrian Thurston <thurston@colm.net>
2017-01-15 11:18:20 -0500

    combined the inline and set-scripts, added inline tear down
    Can now repeatedly set-up and then tear-down using one script pair.

--- src/set-up.sh
+++ src/set-up.sh
@@ -14,3 +14,16 @@ ip link set eth1 up
 ip link set eth2 up
 
 ifconfig filter1 192.168.1.195 netmask 255.255.255.0
+
+for fn in /proc/sys/net/ipv4/conf/*/rp_filter; do echo 0 > $fn; done
+
+iptables -t mangle -N DIVERT
+
+iptables -t mangle -A PREROUTING -p tcp -m socket -j DIVERT
+iptables -t mangle -A DIVERT -j MARK --set-mark 101
+iptables -t mangle -A DIVERT -j ACCEPT
+
+ip rule add fwmark 101 lookup 101
+ip route add local default dev filter1 table 101
+
+iptables -t mangle -A PREROUTING -p tcp -m tcp --dport 443 -j TPROXY  --on-port 4430 --tproxy-mark 101/101
--- src/tear-down.sh
+++ src/tear-down.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+#
 
 set -x 
+
+iptables -t mangle -D PREROUTING -p tcp -m tcp --dport 443 -j TPROXY  --on-port 4430 --tproxy-mark 101/101
+
+ip route del local default dev filter1 table 101
+ip rule del fwmark 101 lookup 101
+
+iptables -t mangle -D PREROUTING -p tcp -m socket -j DIVERT
+iptables -t mangle -D DIVERT -j MARK --set-mark 101
+iptables -t mangle -D DIVERT -j ACCEPT
+
+iptables -t mangle -X DIVERT
+
+for fn in /proc/sys/net/ipv4/conf/*/rp_filter; do echo 0 > $fn; done
+
+###############
 
 ip link set eth2 down
 ip link set eth1 down
