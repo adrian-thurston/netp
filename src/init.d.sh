133817e5f7c24a7e4735f75c3d669d4e0937c157
Adrian Thurston <thurston@colm.net>
2018-02-18 22:14:51 -0500

    support the "live" mode in init.d

--- src/init.d.sh
+++ src/init.d.sh
@@ -5,12 +5,15 @@ pkglibexecdir=@pkglibexecdir@
 sysconfdir=@sysconfdir@
 
 VPN="no"
+LIVE="no"
 [ -f $sysconfdir/interfaces.sh ] && source $sysconfdir/interfaces.sh
 
 # Decide if we are doing a vpn-shuttle up/down or plain up/down.
 root=shuttle
 if [ "x$VPN" = "xyes" ]; then
 	root=vpn
+elif [ "x$LIVE" = "xyes" ]; then
+	root=live
 fi
 
 _start()
