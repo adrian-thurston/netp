d43088aba010f50c6674db202fe942095380471f
Adrian Thurston <thurston@colm.net>
2018-02-19 20:08:10 +0000

    VPN: don't need to setup the connection interface inside namespace
    The tlsproxy can now listen inside the namespace, no need to add connectivity
    there for it to open connections out.

--- src/vpn-down.sh
+++ src/vpn-down.sh
@@ -3,23 +3,39 @@
 
 set -x
 
+# Take down the shuttle inside the namespace.
+ip netns exec inline ifconfig shuttle1 0.0.0.0 down
 ip netns exec inline bash @pkglibexecdir@/shuttle-down
-ip netns exec inline ip route del default via 10.50.1.1 dev con2
-ip netns exec inline ifconfig con2 0.0.0.0 down
-ip netns exec inline ifconfig lo 0.0.0.0 down
-ip netns exec inline ip link set tap0 netns 1
-ip netns exec inline ip link set con2 netns 1
-ip netns exec inline ip link set ep2 netns 1
-ip netns del inline
-ifconfig inline 0.0.0.0 down
-ifconfig ep1 down
-ifconfig con1 down
-brctl delif inline ep1
-brctl delif inline con1
-ip link del ep1 type veth peer name ep1
-ip link del con1 type veth peer name con2
-brctl delbr inline
-sysctl net.ipv4.ip_forward=0
-iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 
+# Deconfigure interior loopback.
+ip netns exec inline ifconfig lo 0.0.0.0 down
+
+# Return interior interfaces to root namespace.
+ip netns exec inline ip link set tap0 netns 1
+ip netns exec inline ip link set pipe1 netns 1
+
+# Destroy namespace.
+ip netns del inline
+
+# Take down outside connection.
+ifconfig pipe0 down
+
+# Remove from bridge.
+brctl delif inline pipe0
+
+# Remove outside veth pair.
+ip link del pipe0 type veth peer name pipe1
+
+# Deconfigure the forwarding.
+iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+iptables -D FORWARD -i inline -o eth0 -j ACCEPT
+iptables -D FORWARD -i eth0 -o inline -j ACCEPT
+
+# Take down the forward interface.
+ifconfig inline 0.0.0.0 down
+
+# Delete the bridge.
+brctl delbr inline
+
+# Stop the VPN.
 start-stop-daemon --stop --pidfile @piddir@/openvpn.pid
--- src/vpn-up.sh
+++ src/vpn-up.sh
@@ -3,26 +3,45 @@
 
 set -x
 
+# start VPN service.
 openvpn --config @pkgdatadir@/openvpn.conf
 
 sleep 1
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# Expected system configuration. Not torn down the -down script.
 sysctl net.ipv4.ip_forward=1
+iptables -P FORWARD DROP
 
+# Add the bridge for outgoing.
 brctl addbr inline
-ip link add ep1 type veth peer name ep2
-ip link add con1 type veth peer name con2
-brctl addif inline ep1
-brctl addif inline con1
-ifconfig con1 up
-ifconfig ep1 up
+
+# Configure it with an IP address.
 ifconfig inline 10.50.1.1 netmask 255.255.255.0 up
+
+# Enable forwarding out.
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -I FORWARD -i inline -o eth0 -j ACCEPT
+iptables -I FORWARD -i eth0 -o inline -j ACCEPT
+
+# Create the veth peer for the connection out.
+ip link add pipe0 type veth peer name pipe1
+
+# Add the outside end to the output bridge.
+brctl addif inline pipe0
+
+# Bring it up. 
+ifconfig pipe0 up
+
+# Create the namespace
 ip netns add inline
-ip link set ep2 netns inline
-ip link set con2 netns inline
+
+# Move interior interfaces to it.
+ip link set pipe1 netns inline
 ip link set tap0 netns inline
+
+# Configure interior loopback.
 ip netns exec inline ifconfig lo 127.0.0.1 up
-ip netns exec inline ifconfig con2 10.50.1.240 netmask 255.255.255.0 up 
-ip netns exec inline ip route add default via 10.50.1.1 dev con2
+
+# Setup the shuttle inside the namespace.
 ip netns exec inline bash @pkglibexecdir@/shuttle-up
+ip netns exec inline ifconfig shuttle1 10.50.1.240 netmask 255.255.255.0 up 
