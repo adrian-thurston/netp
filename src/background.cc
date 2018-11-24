9f276b8a0e3ab681bbffebaeba54b01d328eecfb
Adrian Thurston <thurston@colm.net>
2018-11-24 12:07:59 -0300

    allow pidfile to be skipped, always run with -P
    -P says overwrite pid file no proc found.

--- src/background.cc
+++ src/background.cc
@@ -13,36 +13,36 @@ void on_exit(void)
 	unlink( _PIDFILE );
 }
 
-FILE *pidExists( bool overwritePidFile )
+FILE *pidExists()
 {
 	long pid;
 	FILE *pidf = fopen( _PIDFILE, "r" );
 	if ( pidf != NULL ) {
 		int r = fscanf( pidf, "%ld", &pid );
 		if ( r != 1 )
-			log_FATAL( "pid file exists but we could not read a pid from it: " << _PIDFILE );
+			log_FATAL( "PID file exists but we could not read a pid from it: " << _PIDFILE );
 		fclose( pidf );
 
+		/* Check for a running process. */
 		char fn[64];
 		snprintf( fn, sizeof(fn), "/proc/%ld/comm", pid );
 		fn[sizeof(fn)-1] = 0;
 		FILE *comm = fopen( fn, "rd" );
 		if ( comm == NULL && errno == ENOENT ) {
-			if ( overwritePidFile ) {
-				/* Dangling pid file and overwrite is desired. */
-				log_WARNING( "dangling pid file was overwritten" );
-				return fopen( _PIDFILE, "w" );
-			}
-
-			log_FATAL( "pid file " << _PIDFILE << " is dangling, overwrite with -P" );
+			/* Dangling pid file and overwrite is desired. */
+			log_WARNING( "overwriting a dangling PID file" );
+			return fopen( _PIDFILE, "w" );
+		}
+		else {
+			log_FATAL( "PID file exists and points to a running process" );
 		}
 	}
 
-	log_FATAL( "could not open pid file: " << _PIDFILE << ": " << strerror(EEXIST) );
+	log_FATAL( "could not open PID file: " << _PIDFILE << ": " << strerror(EEXIST) );
 	return NULL;
 }
 
-FILE *openPidFile( bool overwritePidFile )
+FILE *openPidFile()
 {
 	/* NOTE: using a glibc extension here to get an exclusive open. */
 	FILE *pidf = fopen( _PIDFILE, "wx" );
@@ -50,7 +50,7 @@ FILE *openPidFile( bool overwritePidFile )
 		/* If the pid file creation failed because the file exists then
 		 * open it up and investigate a bit. User can explicitly
 		 * overwrite it. */
-		pidf = pidExists( overwritePidFile );
+		pidf = pidExists();
 	}
 
 	if ( pidf == NULL )
@@ -71,11 +71,16 @@ void writePidFile( FILE *pidf )
 	fclose( pidf );
 }
 		
-void maybeBackground( bool background, bool overwritePidFile )
+void maybeBackground( bool background, bool usePid )
 {
+	if ( background && !usePid )
+		log_FATAL( "cannot run in the background with no PID file" );
+
 	if ( !background ) {
-		FILE *pidf = openPidFile( overwritePidFile );
-		writePidFile( pidf );
+		if ( usePid ) {
+			FILE *pidf = openPidFile();
+			writePidFile( pidf );
+		}
 	}
 	else {
 		/* Ensure we can open the files we need to open. */
@@ -88,7 +93,7 @@ void maybeBackground( bool background, bool overwritePidFile )
 		if ( lgfd == -1 )
 			log_FATAL( "daemonize: could not open log file: " << strerror(errno) );
 
-		FILE *pidf = openPidFile( overwritePidFile );
+		FILE *pidf = openPidFile();
 
 		/* Change dirs, but don't close fds. */
 		int result = daemon( 0, 1 );
